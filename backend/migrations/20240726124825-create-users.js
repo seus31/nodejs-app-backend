36c933ad31a1318435f5da10f00a14e522d4d258
seus31 <us.balut.big.dipper@gmail.com>
2024-07-26 23:53:25 +0900

    add: マイグレーションの追加

---
+++ backend/migrations/20240726124825-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * ≈
+     */
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: Sequelize.STRING,
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users');
+  }
+};
