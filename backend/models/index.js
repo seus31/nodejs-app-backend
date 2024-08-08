4826cbd29f0483e1b2889659eb2d62fd854f4997
seus31 <us.balut.big.dipper@gmail.com>
2024-08-09 00:24:56 +0900

    fix: taskモデルの読み込み

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const User = require('./userModel')
+const User = require('./user')
+const Task = require('./task')
 
 module.exports = fp(async function (fastify, opts) {
   const sequelize = fastify.sequelize
 
   const models = {
     User: User(sequelize),
+    Task: Task(sequelize),
   }
 
   fastify.decorate('models', models)
