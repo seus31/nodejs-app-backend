7b53be2d68055c5a47beb869e80f2af5c7f36294
seus31 <us.balut.big.dipper@gmail.com>
2024-08-10 16:44:28 +0900

    add: バックエンドにcors設定

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,17 @@
 const path = require('node:path')
 const AutoLoad = require('@fastify/autoload')
 require('dotenv').config()
+const cors = require('@fastify/cors');
 
 const options = {}
 
 module.exports = async function (fastify, opts) {
+  fastify.register(cors, {
+    origin: ['http://localhost:4200', 'http://localhost:4000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  });
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
