a459629112bff9e00db2f7b80d739ccb1a325fc0
seus31 <us.balut.big.dipper@gmail.com>
2024-08-10 16:45:03 +0900

    add: environmentにapiUrlの設定追加

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: false,
+  apiUrl: 'http://localhost:4000/api/v1'
 };
