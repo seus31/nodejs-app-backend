ccc61a59e227cbe425d8eda37af94238b9e19c11
seus31 <us.balut.big.dipper@gmail.com>
2024-09-02 01:06:13 +0900

    タスク一覧取得APIのコントローラーメソッド追加

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,10 +1,17 @@
 const taskService = require('../services/taskService');
+const userService = require("../services/userService");
 
 async function createTask(fastify, req, reply) {
   const newTask = await taskService.createTask(fastify, req.body);
   reply.code(201).send(newTask);
 }
 
+async function getTasks(fastify, req, reply){
+  const tasks = await taskService.getTasks(fastify, req.query);
+  reply.code(200).send(tasks);
+}
+
 module.exports = {
   createTask,
+  getTasks,
 };
